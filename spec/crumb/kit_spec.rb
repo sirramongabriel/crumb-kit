a0c43efd45944a324b54d56a68181e8c3dd2c6e1
sirramongabriel <sirramongabriel@gmail.com>
2025-04-11 10:39:21 -0400

    Initial commit of gem structure

---
+++ spec/crumb/kit_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Crumb::Kit do
+  it "has a version number" do
+    expect(Crumb::Kit::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
